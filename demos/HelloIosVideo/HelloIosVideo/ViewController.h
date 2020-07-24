e3c9f4301ff0f8e42175fc4c88e3c6d00413311c
Miguel Pari Soto <miguel@idwall.co>
2020-07-24 02:07:52 -0300

    fix(Hello..): adjust for compile HelloIosVideo

--- demos/HelloIosVideo/HelloIosVideo/ViewController.h
+++ demos/HelloIosVideo/HelloIosVideo/ViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-#import <opencv2/highgui/cap_ios.h>
+#import <opencv2/videoio/cap_ios.h>
 using namespace cv;
 
 @interface ViewController : UIViewController<CvVideoCameraDelegate>
--- demos/HelloIosVideo/HelloIosVideo/ViewController.mm
+++ demos/HelloIosVideo/HelloIosVideo/ViewController.mm
@@ -64,11 +64,11 @@
     // Do some OpenCV stuff with the image
     
     Mat image_copy;
-    cvtColor(image, image_copy, CV_BGRA2BGR);
+    cvtColor(image, image_copy, cv::COLOR_BGRA2BGR);
     
     // invert image
     bitwise_not(image_copy, image_copy);
-    cvtColor(image_copy, image, CV_BGR2BGRA);
+    cvtColor(image_copy, image, cv::COLOR_BGR2BGRA);
 }
 #endif
 
